545f2d293530ebf8abe3075554895758041078f6
Zackery Griesinger <zgriesinger@gmail.com>
2025-03-28 21:57:43 -0500

    build: add back in neovim

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,35 +1,35 @@
 return {
-  -- {
-  --   'zbirenbaum/copilot.lua',
-  --   cmd = 'Copilot',
-  --   opts = {
-  --     panel = {
-  --       enabled = true,
-  --     },
-  --     suggestion = {
-  --       enabled = true,
-  --       auto_trigger = true,
-  --       keymap = {
-  --         accept = '<C-l>',
-  --         accept_word = false,
-  --         accept_line = false,
-  --         next = '<C-J>',
-  --         prev = '<C-K>',
-  --         dismiss = '<C-]>',
-  --       },
-  --     },
-  --     filetypes = {
-  --       yaml = true,
-  --       markdown = true,
-  --       help = true,
-  --       gitcommit = true,
-  --       gitrebase = true,
-  --       hgcommit = true,
-  --       svn = true,
-  --       cvs = true,
-  --       ['.'] = true,
-  --     },
-  --   },
-  --   event = 'InsertEnter',
-  -- },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    opts = {
+      panel = {
+        enabled = true,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = '<C-l>',
+          accept_word = false,
+          accept_line = false,
+          next = '<C-J>',
+          prev = '<C-K>',
+          dismiss = '<C-]>',
+        },
+      },
+      filetypes = {
+        yaml = true,
+        markdown = true,
+        help = true,
+        gitcommit = true,
+        gitrebase = true,
+        hgcommit = true,
+        svn = true,
+        cvs = true,
+        ['.'] = true,
+      },
+    },
+    event = 'InsertEnter',
+  },
 }
