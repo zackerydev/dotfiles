020ea41cbb75fcc305ee5b3fa4f67b2c4d7522a9
Zackery Griesinger <zgriesinger@gmail.com>
2025-08-18 09:14:53 -0500

    build: single file neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,21 @@
+-- Globals
+local icons = {
+  spinner = { '', '', '', '', '', '', '', '', '', '', '', '', '' },
+  diagnostics = {
+    Error = ' ',
+    Warning = ' ',
+    Info = ' ',
+    Hint = ' ',
+  },
+  git = {
+    LineAdded = '',
+    LineModified = '',
+    LineRemoved = '',
+    LineLeft = '▎',
+    LineMiddle = '│',
+  },
+}
+
 -- Configuration
 vim.o.clipboard = 'unnamedplus'
 vim.o.expandtab = true
@@ -10,8 +28,11 @@ vim.o.cmdheight = 0
 vim.o.signcolumn = 'no'
 vim.o.laststatus = 3
 vim.o.termguicolors = false
+vim.o.winborder = 'rounded'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
 
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 -- Keybinds
 vim.keymap.set('n', '<leader>o', ':Neotree toggle=true<CR>', { silent = true })
@@ -26,6 +47,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move Left' })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move Down' })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move Left' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move Up' })
+vim.keymap.set('n', '<leader>c', ':e ~/.config/nvim/init.lua', { desc = 'Edit Neovim Configuration' })
 
 -- Picker
 vim.keymap.set('n', '<leader>ff', ':Pick files<cr>', { desc = 'Find Files' })
@@ -43,40 +65,232 @@ vim.keymap.set('n', '<leader>tca', ':Trouble qflist toggle<CR>', { desc = 'Troub
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 -- Packages
-vim.pack.add({
-  { src = "https://github.com/nvim-lua/plenary.nvim" },
-  { src = "https://github.com/MunifTanjim/nui.nvim" },
-  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-  { src = "https://github.com/nvim-neo-tree/neo-tree.nvim", version = "v3.x" },
-  { src = "https://github.com/echasnovski/mini.nvim" },
-  { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/folke/trouble.nvim" },
-  { src = "https://github.com/neanias/everforest-nvim" },
-})
-
--- Plugins
-require 'everforest'.setup {}
-require 'mason'.setup {}
-require 'trouble'.setup {}
-require 'mini.pick'.setup {}
-require 'mini.basics'.setup {
-  options = {
-    extra_ui = true,
-    win_borders = 'rounded'
-  },
-  autocommands = {
-    relnum_in_visual_mode = true
-  }
+vim.pack.add {
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/MunifTanjim/nui.nvim' },
+  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = 'v3.x' },
+  { src = 'https://github.com/echasnovski/mini.nvim' },
+  { src = 'https://github.com/mason-org/mason.nvim' },
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/folke/trouble.nvim' },
+  { src = 'https://github.com/neanias/everforest-nvim' },
+  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+  { src = 'https://github.com/stevearc/conform.nvim' },
+  { src = 'https://github.com/nvim-lualine/lualine.nvim' },
+  { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+  { src = 'https://github.com/saghen/blink.cmp', version = 'v1.6.0' },
+  { src = 'https://github.com/folke/snacks.nvim' },
+  { src = 'https://github.com/chrisgrieser/nvim-origami' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
 }
 
-vim.lsp.enable({ "lua_ls" })
+-- Plugins
+require('everforest').setup {}
+require('mason').setup {}
+require('trouble').setup {}
+require('origami').setup {}
+require('mini.pick').setup {}
+require('mini.basics').setup {
+  options = {
+    extra_ui = true,
+    win_borders = 'rounded',
+  },
+  autocommands = {
+    relnum_in_visual_mode = true,
+  },
+}
+require('mason-lspconfig').setup {
+  ensure_installed = { 'ts_ls', 'biome', 'lua_ls', 'gopls', 'helm-ls' },
+}
+require('nvim-treesitter').setup {}
+require('lualine').setup {
+  options = {
+    theme = 'auto',
+    -- globalstatus = true,
+    section_separators = '',
+    component_separators = '',
+    disabled_filetypes = { 'mason', 'lazy', 'NvimTree' },
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = {
+      { 'filename', path = 1 },
+      {
+        'lsp_status',
+        icon = '', -- f013
+        symbols = {
+          spinner = icons.spinner,
+          done = false,
+          separator = ' ',
+        },
+        -- List of LSP names to ignore (e.g., `null-ls`):
+        ignore_lsp = {},
+      },
+    },
+    lualine_x = {
+      {
+        'diff',
+        source = function()
+          local gitsigns = vim.b.gitsigns_status_dict
+          if gitsigns then
+            return {
+              added = gitsigns.added,
+              modified = gitsigns.changed,
+              removed = gitsigns.removed,
+            }
+          end
+        end,
+        symbols = {
+          added = icons.git.LineAdded .. ' ',
+          modified = icons.git.LineModified .. ' ',
+          removed = icons.git.LineRemoved .. ' ',
+        },
+        colored = true,
+        always_visible = false,
+      },
+      {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        sections = { 'error', 'warn', 'info', 'hint' },
+        symbols = {
+          error = icons.diagnostics.Error,
+          hint = icons.diagnostics.Hint,
+          info = icons.diagnostics.Info,
+          warn = icons.diagnostics.Warning,
+        },
+        colored = true,
+        update_in_insert = false,
+        always_visible = false,
+      },
+      {
+        'filetype',
+        icon_only = true,
+      },
+    },
+    lualine_y = {},
+    lualine_z = {},
+  },
+}
 
-vim.cmd("colorscheme everforest")
-vim.cmd(":hi statusline guibg=NONE")
+require('conform').setup {
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 2500,
+    lsp_fallback = true,
+  },
+  formatters = {
+    ['biome-check'] = {
+      args = { 'check', '--stdin-file-path', '$FILENAME', '--fix' },
+    },
+  },
+  formatters_by_ft = {
+    lua = { 'stylua' },
+    go = { 'goimports', 'gofmt' },
+    html = { 'prettier' },
+    htmlangular = { 'prettier' },
+    htmldjango = { 'prettier' },
+    yaml = { 'prettier' },
+    markdown = { 'prettier' },
+    astro = { 'prettier' },
+    -- Use a sub-list to run only the first available formatter
+    javascript = { 'biome' },
+    javascriptreact = { 'biome' },
+    typescript = { 'biome' },
+    typescriptreact = { 'biome' },
+    rust = { 'rustfmt' },
+  },
+}
 
+require('blink.cmp').setup {
+  keymap = {
+    preset = 'default',
+    ['<C-k>'] = { 'select_prev', 'show_documentation', 'fallback' },
+    ['<C-j>'] = { 'select_next', 'show_documentation', 'fallback' },
+    ['<Tab>'] = {
+      function(cmp)
+        if cmp.snippet_active() then
+          return cmp.accept()
+        else
+          return cmp.select_and_accept()
+        end
+      end,
+      'select_next',
+      'show_documentation',
+      'fallback',
+    },
+    ['<S-Tab>'] = { 'select_prev', 'fallback' },
+    ['<C-Space>'] = { 'show', 'show_documentation', 'fallback' },
+    ['<C-e>'] = { 'hide', 'fallback' },
+    ['<C-d>'] = { 'scroll_documentation_up', 'fallback' },
+    ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+  },
+  appearance = {
+    nerd_font_variant = 'mono',
+  },
+  fuzzy = {
+    implementation = 'rust',
+    prebuilt_binaries = {
+      force_version = 'v1.6.0',
+    },
+  },
+  completion = {
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 500,
+    },
+  },
+  sources = {
+    default = { 'lsp', 'path' },
+  },
+}
 
+require('snacks').setup {
+  image = {},
+  notifier = {},
+  statuscolumn = {},
+  zen = {},
+}
 
+vim.lsp.enable { 'lua_ls' }
+
+local function lspSymbol(name, icon)
+  local hl = 'DiagnosticSign' .. name
+  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+end
+
+lspSymbol('Error', '󰅙')
+lspSymbol('Info', '󰋼')
+lspSymbol('Hint', '󰌵')
+lspSymbol('Warn', '')
+
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = '',
+  },
+  signs = true,
+  underline = false,
+  update_in_insert = false,
+}
+
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = 'single',
+  syntax = 'markdown',
+})
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = 'single',
+  syntax = 'markdown',
+})
+-- Disable LSP Highlighting
+for _, group in ipairs(vim.fn.getcompletion('@lsp', 'highlight')) do
+  vim.api.nvim_set_hl(0, group, {})
+end
+
+vim.treesitter.language.register('markdown', { 'md', 'mdx' })
+
+vim.cmd 'colorscheme everforest'
+vim.cmd ':hi statusline guibg=NONE'
 
 -- require 'config.settings'
 -- require 'config.lazy'
